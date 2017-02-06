b00dec11e057820cf0a861c41dbb2f5c95822e9c
Alpana Toteja <atoteja@Alpanas-MacBook-Pro-3.local>
2017-02-05 21:40:07 -0500

    Commit

--- 206W17_HW3.py
+++ 206W17_HW3.py
@@ -22,7 +22,7 @@ import re
 ## Write code to define your parse_counted_words function here.
 
 def parse_counted_words(s):
-    list_it = re.findall(r'\b\d+\s\D+\b', s)   #looks at digits (1 or more), space, character (1 or more)
+    list_it = re.findall(r'\b\d+\s\D+\b', s)   #looks at digits (1 or more), space, non-digit (1 or more)
     #split_list = tuple_list.split()
     if len(list_it) == 0:
         return None
@@ -35,23 +35,39 @@ def parse_counted_words(s):
         return return_tuple
 
 
-print(parse_counted_words('goldilocks and the 3 little pigs'))
-print(parse_counted_words('678 1234567 890  '))
-print(parse_counted_words("hellllo 5000"))
-print(parse_counted_words('5 watermelons, 13 pineapples, and 1 papaya.'))
+#print(parse_counted_words('goldilocks and the 3 little pigs'))
+#print(parse_counted_words('678 1234567 890  '))
+#print(parse_counted_words("hellllo 5000"))
+#print(parse_counted_words('5 watermelons, 13 pineapples, and 1 papaya.'))
 ## PART 2: 200 points
 
 ## We have provided a text file computer_paths.txt. It's not incredibly long -- you can scan through it, but do NOT hard code your answers! Each line contains 1 filesystem path.
 
 ## (a) Write Python code to determine how many of these paths identify FILES, not directories. Save that number in the variable file_paths_num.
 
+f = open("/Users/atoteja/Desktop/Homework3/computer_paths.txt", 'r')
+data = f.read()
+
+files_list = re.findall(r'\.[a-z]', data, re.MULTILINE)
+
+file_paths_num = len(files_list)
+
+
 ## (b) Write Python code to determine how many of these paths are FULL paths, not relative paths. Save that number in the variable full_paths_num.
+#full_paths = re.findall(r'\.[a-z]', data)
+full_paths = re.findall(r'(\$/.*?\.\S*)', data)
+print(full_paths)
 
 ## (c) Write Python code to determine how many of these paths describe a Python file saved inside a folder called SI206. Save that number in the variable python_course_paths.
 
+new_list = re.findall(r'\/[A-Z][a-z]{1,5}\/[A-Z][a-z]{1,6}\/[A-Z][a-z]{1,9}\/[A-Z]{1,3}[0-9]{1,3}\/', data)
+python_course_paths = len(new_list)
+
+
 ## (d) Write Python code to determine how many of these paths describe a Microsoft file (a file that EITHER ends with .docx OR .xlsx, but nothing else counts) where the file name ends in a digit. Save that total in the variable microsoft_files_num.
 
-
+d = re.findall(r'\d\.docx|\d\.xlsx', data)
+microsoft_files_num = len(d)
 
 
 
@@ -80,7 +96,7 @@ class Part1_HW3(unittest.TestCase):
         self.assertEqual(parse_counted_words("!!!!! 72 and 76 calendars"),('76',"calendars"))
 
 class Part2_HW3(unittest.TestCase):
-    def test_cpaths_1(self):
+    def test_cpaths_1(self): 
         self.assertEqual(file_paths_num,16)
     def test_cpaths_2(self):
         self.assertEqual(full_paths_num,16)
